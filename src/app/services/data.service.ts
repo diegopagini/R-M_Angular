1dad163d2cabc3ac2255c342a598a7cbada9f097
Diego Pagini <diegopagini777@gmail.com>
2020-12-15 07:10:51 -0300

    not found ready

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Console } from 'console';
 import { Observable } from 'rxjs';
 import { Character } from '../interfaces/character';
 
