1cd9ff07d15ef712c446ef9f1a09ee57a7d6994d
Diego Pagini <diegopagini777@gmail.com>
2020-12-15 06:58:58 -0300

    hay que hacer andar el card individual de personajes

--- src/app/character-card/character-card.component.ts
+++ src/app/character-card/character-card.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-character-card',
   templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.scss']
+  styleUrls: ['./character-card.component.scss'],
 })
 export class CharacterCardComponent implements OnInit {
-
-  constructor() { }
+  idCharacter;
+  constructor(private activeRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.idCharacter = this.activeRoute.snapshot.params.id;
   }
-
 }
